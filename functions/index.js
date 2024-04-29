d16fc595a619e30e4233117bcde9633622f1a861
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-04-29 06:07:54 +0000

    First Functions: Test

--- functions/index.js
+++ functions/index.js
@@ -9,17 +9,33 @@
 
 // const {onRequest} = require("firebase-functions/v2/https");
 const {onCall} = require("firebase-functions/v2/https");
+const { google } = require('googleapis');
 
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
-logger.info("hello logs!");
 exports.helloWorld = onCall({cors: true}, (request) => {
   logger.info("Hello logs!", {structuredData: true});
   return {
     message: "Hello World from Firebase Functions!",
   };
+});
+
+
+
+exports.getYoutubeInfo = onCall(async (data, context) => {
+  const accessToken = data.accessToken;
+  // const apiKey = functions.config().youtube.apikey;
+
+  // YouTube Data API 사용
+  const youtube = google.youtube({ version: 'v3', auth: apiKey });
+  const response = await youtube.channels.list({
+    part: 'snippet,contentDetails,statistics',
+    mine: true,
+    access_token: accessToken
+  });
+  return response.data;
 });
 
 
